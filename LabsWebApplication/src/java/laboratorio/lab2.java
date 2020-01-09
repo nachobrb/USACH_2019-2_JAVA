7d33af492db83144328979478a5180ce73cf881c
Ignacio Olave <olave.ignacio.a@gmail.com>
2020-01-09 10:03:20 -0300

    lab2 OK

--- LabsWebApplication/src/java/laboratorio/lab2.java
+++ LabsWebApplication/src/java/laboratorio/lab2.java
@@ -5,11 +5,52 @@
  */
 package laboratorio;
 
+import static java.lang.Double.max;
+import java.lang.*;
+
+
 /**
  *
  * @author ignacio
  */
+
 public class lab2 {
+    public static String contar(String str) {
+        if (str.length() == 0) {
+            return "";
+        }
+
+        int maxima_ocurrencia = 0;
+        String caracter = new String();
+        int j = 0;
+        int count = 0;
+        
+        for (j = 0;j < 10;j++) {
+            count = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (("" + str.charAt(i)).equals(String.valueOf(j)) == true) {
+                   count++;
+                }
+            }
+            if (count > maxima_ocurrencia) {
+                maxima_ocurrencia = count;
+                caracter = String.valueOf(j);
+            }
+        }
+        return "Máxima ocurrencia " + str + ": Número = " + caracter + ", ocurrencia = " + String.valueOf(maxima_ocurrencia);
+    }
+    public static int esPar(double n) {
+        if(n == 0) return 0;
+        else {
+            if(n % 2 == 0.0) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+    }
+    
     public static String pregunta1(String btn){
         if(btn != null) {
             int[][] matriz = new int[10][10];
@@ -63,8 +104,20 @@ public class lab2 {
     }
     
     public static String pregunta2(String c1, String c2, String c3, String c4, String c5){
-        if(c1 == null ||c2 == null ||c3 == null ||c4 == null ||c5 == null){
-            return "";
+        if(c1 == null) {
+            c1 = "";
+        }
+        if(c2 == null) {
+            c2 = "";
+        }
+        if(c3 == null) {
+            c3 = "";
+        }
+        if(c4 == null) {
+            c4 = "";
+        }
+        if(c5 == null) {
+            c5 = "";
         }
         return (
             "<div class=\"container\">" +
@@ -80,13 +133,163 @@ public class lab2 {
             "</div>"
         );
     }
-    
+
+
     public static String pregunta3(String c1, String c2, String c3){
-        if(c1 == null ||c2 == null ||c3 == null){
-            return "";
+        if(c1 == null) {
+            c1 = "";
+        }
+        if(c2 == null) {
+            c2 = "";
+        }
+        if(c3 == null) {
+            c3 = "";
         }
         return (
-           ""
+            "<div class=\"container\">" +
+                "<div class=\"md-form mb-4 pink-textarea active-pink-textarea disabled\">" +
+                    "<textarea id=\"form7\" class=\"md-textarea form-control\" rows=\"4\">" +
+                        contar(c1) + 
+                        "\n" + contar(c2) + 
+                        "\n" + contar(c3) + 
+                    "</textarea>" +
+                "</div>" +  
+            "</div>"
         );
     }
+
+    public static String pregunta4(String c1, String c2, String c3, String c4){
+        if(c1 == null) {
+            c1 = "";
+        }
+        if(c2 == null) {
+            c2 = "";
+        }
+        if(c3 == null) {
+            c3 = "";
+        }
+        if(c4 == null) {
+            c4 = "";
+        }
+        String str = new String();
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
+        int count4 = 0;
+        int countMax = 0;
+
+        for (int i = 0;i < c1.length();i++){
+            if (esPar(( Integer.parseInt("" + c1.charAt(i)))) == 1){
+                count1++;
+            }
+        }
+        for (int i = 0;i < c2.length();i++){
+            if (esPar(( Integer.parseInt("" + c2.charAt(i)))) == 1){
+                count2++;
+            }
+        }
+        for (int i = 0;i < c3.length();i++){
+            if (esPar(( Integer.parseInt("" + c3.charAt(i)))) == 1){
+                count3++;
+            }
+        }
+        for (int i = 0;i < c4.length();i++){
+            if (esPar(( Integer.parseInt("" + c4.charAt(i)))) == 1){
+                count4++;
+            }
+        }
+        countMax = (int) max(max(max(count1,count2),count3),count4);
+
+        if(count1 == countMax) {
+            str =  str + "Número = " + c1 + ", cantidad = " + countMax + "\n";
+        }
+        if(count2 == countMax) {
+            str =  str + "Número = " + c2 + ", cantidad = " + countMax + "\n";
+        }
+        if(count3 == countMax) {
+            str =  str + "Número = " + c3 + ", cantidad = " + countMax + "\n";
+        }
+        if(count4 == countMax) {
+            str =  str + "Número = " + c4 + ", cantidad = " + countMax + "\n";
+        }
+
+
+        return (
+            "<div class=\"container\">" +
+                "<div class=\"md-form mb-4 pink-textarea active-pink-textarea disabled\">" +
+                    "<textarea id=\"form7\" class=\"md-textarea form-control\" rows=\"4\">" +
+                        str + 
+                    "</textarea>" +
+                "</div>" +  
+            "</div>"
+        );
+    }
+
+    public static String ecripta(String str, int n){
+        String strTmp = str.toUpperCase();
+        String newStr = new String();
+        
+        for(int i = 0;i < strTmp.length();i++) {
+            if((int)(strTmp.charAt(i)) <= 90 - n){
+                char tmp = (char) (str.charAt(i) + n);
+                String tmp2 = "" + tmp;
+                newStr = newStr + tmp2;
+            }
+            else {
+                char tmp = (char) (str.charAt(i) + n - 26);
+                String tmp2 = "" + tmp;
+                newStr = newStr + tmp2;
+            }
+        }
+        return newStr;
+    }
+
+
+    public static String pregunta5(String c1, String c2, String c3, String c4, String c5){
+        String str = new String();
+
+        if(c1 == null) {
+            c1 = "";
+        }
+        if(c2 == null) {
+            c2 = "";
+        }
+        if(c3 == null) {
+            c3 = "";
+        }
+        if(c4 == null) {
+            c4 = "";
+        }
+        if(c5 == null) {
+            c5 = "";
+        }
+
+        if(c1 != "") {
+            str =  str + "Cadena RAW = " + c1 + ", Encriptada = " + ecripta(c1,5) + "\n";
+        }
+        if(c2 != "") {
+            str =  str + "Cadena RAW = " + c2 + ", Encriptada = " + ecripta(c2,5) + "\n";
+        }
+        if(c3 != "") {
+            str =  str + "Cadena RAW = " + c3 + ", Encriptada = " + ecripta(c3,5) + "\n";
+        }
+        if(c4 != "") {
+            str =  str + "Cadena RAW = " + c4 + ", Encriptada = " + ecripta(c4,5) + "\n";
+        }
+        if(c5 != "") {
+            str =  str + "Cadena RAW = " + c5 + ", Encriptada = " + ecripta(c5,5) + "\n";
+        }
+
+
+        return (
+            "<div class=\"container\">" +
+                "<div class=\"md-form mb-4 pink-textarea active-pink-textarea disabled\">" +
+                    "<textarea id=\"form7\" class=\"md-textarea form-control\" rows=\"4\">" +
+                        str + 
+                    "</textarea>" +
+                "</div>" +  
+            "</div>"
+        );
+    }
+
 }
